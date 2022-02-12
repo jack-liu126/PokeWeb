40e50469f2a14fdba83fc8674d3021333293caff
Jack Liu <jackliou10@gmail.com>
2022-02-12 14:35:11 +0800

    sqldb set ok

---
+++ PokeWeb/DbContext/PokeContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PokeWeb
+{
+    public partial class PokeContext : DbContext
+    {
+        public PokeContext()
+        {
+
+        }
+
+        public PokeContext(DbContextOptions<PokeContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //當發現沒有連結字串的時候才會跑進去
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=All");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
