1b5728e55abeb2a4b0dedbec14594acd09cbeef0
Jack Liou <Jack Liou@DESKTOP-3KSOQ1B>
2022-02-12 14:18:43 +0800

    加入專案檔案。

---
+++ PokeWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PokeWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
